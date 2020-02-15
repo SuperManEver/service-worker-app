d8d5dab6fb5f74f6b23328b7204260ed9df7e5f4
Nick Luparev <nikita.luparev@gmail.com>
2020-02-16 00:53:29 +0500

    messaging between SW and app

--- web/js/blog.js
+++ web/js/blog.js
@@ -24,11 +24,13 @@
     window.addEventListener("online", function() {
       offlineIcon.classList.add("hidden");
       isOnline = true;
+      sendStatusUpdate();
     });
 
     window.addEventListener("offline", function() {
       offlineIcon.classList.remove("hidden");
       isOnline = false;
+      sendStatusUpdate();
     });
   }
 
@@ -46,7 +48,36 @@
       "controllerchange",
       function onControllerChange() {
         svcworker = navigator.serviceWorker.controller;
+        sendStatusUpdate(svcworker);
       }
     );
+
+    navigator.serviceWorker.addEventListener("message", onSWMessage);
+  }
+
+  function onSWMessage(evt) {
+    const { data } = evt;
+
+    if (data.requestStatusUpdate) {
+      const msg =
+        "Received status update request from service worker, responding...";
+      console.log(msg);
+      sendStatusUpdate(evt.ports && evt.ports[0]);
+    }
+  }
+
+  function sendStatusUpdate(target) {
+    const payload = { statusUpdate: { isOnline, isLoggedIn } };
+    sendSWMessage(payload, target);
+  }
+
+  function sendSWMessage(msg, target) {
+    if (target) {
+      target.postMessage(msg);
+    } else if (svcworker) {
+      svcworker.postMessage(msg);
+    } else {
+      navigator.serviceWorker.controller.postMessage(msg);
+    }
   }
 })();
--- web/js/sw.js
+++ web/js/sw.js
@@ -1,17 +1,33 @@
 "use strict";
 
 // TODO
-const version = 1;
+const version = 2;
+var isOnline = true;
+var isLoggedIn = false;
 
 self.addEventListener("install", onInstall);
 self.addEventListener("activate", onActivate);
+self.addEventListener("message", onMessage);
 
 main().catch(console.error);
 
 // *********************************************************
 
 async function main() {
-  console.log(`Service worker (${version}) is started...`);
+  await sendMessage({ requestStatusUpdate: true });
+}
+
+async function sendMessage(payload) {
+  // because we potentially can have many clients
+  var allClients = await clients.matchAll({ includeUncontrolled: true });
+
+  return Promise.all(
+    allClients.map(function clientMsg(client) {
+      var chan = new MessageChannel();
+      chan.port1.onmessage = onMessage;
+      return client.postMessage(payload, [chan.port2]);
+    })
+  );
 }
 
 async function onInstall(evt) {
@@ -26,4 +42,14 @@ async function onActivate(evt) {
 async function handleActivation() {
   await clients.claim();
   console.log("Actived");
+}
+
+function onMessage({ data }) {
+  if (data.statusUpdate) {
+    ({ isOnline, isLoggedIn } = data.statusUpdate);
+
+    console.log(
+      `Service Worker (v${version}) status update, isOnline: ${isOnline}, isLoggedIn: ${isLoggedIn}`
+    );
+  }
 }
